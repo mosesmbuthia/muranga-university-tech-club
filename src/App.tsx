172bd605fb7ad07602733c827b0120c5c4f5dfbd
mosesmbuthia <mbuthiamoses435@gmail.com>
2025-03-21 18:59:59 +0300

    performed the poject scaffolding using react and typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <div className="app">
+      <h1>This is Muranga University Tech Club</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
