5d4f330431313eb6b0200a6e435dae8796d014fe
mosesmbuthia <mbuthiamoses435@gmail.com>
2025-03-26 15:07:13 +0300

    build the footer section

--- src/assets/Components/Pages/Home/Footer/Footer.tsx
+++ src/assets/Components/Pages/Home/Footer/Footer.tsx
@@ -1,10 +1,60 @@
-
+import { FaFacebook, FaInstagram, FaTwitter,FaLinkedin } from "react-icons/fa";
 import "./Footer.css"
 function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <section>
+      <div className="footer-container">
+      <footer className="footer">
+      <div className="footer-container">
+        <h2 className="club">Murang'a University Tech Club</h2>
+
+        <div className="footer-links">
+          <div className="footer-column">
+            <h3>Quick Links</h3>
+            <ul>
+              <li>Overview</li>
+              <li>Features</li>
+              <li>Solutions</li>
+              <li>Courses</li>
+            </ul>
+          </div>
+
+          <div className="footer-column">
+            <h3>Club</h3>
+            <ul>
+              <li>About us</li>
+              <li>Membership</li>
+              <li>Activities</li>
+              <li>Connect</li>
+            </ul>
+          </div>
+
+          <div className="footer-column">
+            <h3>Resource</h3>
+            <ul>
+              <li>Leadership</li>
+              <li>Tracks</li>
+              <li>Events</li>
+              <li>Help center</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="footer-bottom">
+          <p>Copyright © 2024 Moses Mbuthia. All Rights Reserved.</p>
+          <div className="social-icons">
+            <FaFacebook />
+            <FaInstagram />
+            <FaTwitter />
+            <FaLinkedin />
+            
+            
+          </div>
+        </div>
+      </div>
+    </footer>
+      </div>
+    </section>
   )
 }
 
