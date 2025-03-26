f5c3270ff87b0bfea139a7372353a1f0bf98ffe3
mosesmbuthia <mbuthiamoses435@gmail.com>
2025-03-26 10:29:49 +0300

    modified the tracks section

--- src/assets/Components/Pages/Tracks/Tracks.tsx
+++ src/assets/Components/Pages/Tracks/Tracks.tsx
@@ -3,6 +3,7 @@ import uiuxImg from "../../../images/uiux.jpg"
 import webImg from "../../../images/webdevelepoment.png"
 import mobileImg from "../../../images/mobile app.jpg"
 import cloudengImg from "../../../images/cloudeng.jpg"
+import powerBiImg from "../../../images/powebi.png"
 import "./Tracks.css"
 function Tracks() {
   return (
@@ -94,15 +95,15 @@ function Tracks() {
 
         <div className="track-card">
           <img
-            src={cyberTrackImg}
+            src={powerBiImg}
             alt="Cyber Security"
             className="track-image"
           />
           <div className="track-content">
             <span className="track-category">It & Computer Science</span>
-            <h2 className="track-title">Cyber Security</h2>
+            <h2 className="track-title">Power Platform</h2>
             <div className="track-description">
-              Students who choose the Cyber Security track will delve into the critical aspects of protecting systems, networks, and data from cyber threats. Throughout this track, they will learn about various security protocols, ethical hacking, encryption techniques, and threat analysis. They will engage in hands-on projects and real-world scenarios to apply their knowledge in identifying and mitigating security vulnerabilities. By the end of the track, students will be equipped with the skills to safeguard digital environments, conduct security audits, and implement robust security measures to protect against cyber attacks.
+            Students who select the Power Platform track will explore the capabilities of Microsoft's Power Platform, which includes Power BI, Power Apps, Power Automate, and Power Virtual Agents. They will learn how to create custom business applications, automate workflows, analyze data, and build chatbots with minimal coding. Through hands-on projects and interactive workshops, students will gain practical experience in leveraging these tools to enhance business processes and drive data-driven decision-making. By the end of the track, students will be proficient in developing integrated solutions using the Power Platform, enabling them to transform organizational operations and improve efficiency.
             </div>
             <button className="learn-more">Learn More →</button>
           </div>
